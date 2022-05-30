4fd5a28af2269358a13f5a672c09192fd1523831
takenokonyokki <97602717+takenokonyokki@users.noreply.github.com>
2022-05-30 14:05:55 +0000

    スライドショー

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -3,3 +3,18 @@ $(document).ready(function () {
     $(this).css('color','red');
   });
 });
+
+$(document).ready(function () {
+  $("#theTarget").skippr({
+    transition : 'fade',
+    speed : 1000,
+    easing : 'easeOutQuart',
+    navType : 'block',
+    childrenElementType : 'div',
+    arrows : true,
+    autoPlay : true,
+    autoPlayDuration : 3000,
+    keyboardOnAlways : true,
+    hidePrevious : false
+  });
+});
